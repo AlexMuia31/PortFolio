8d87f5544da89dab6a25bdb6a2e7124c5963258d
AlexMuia31 <alexmuia31@gmail.com>
2019-12-19 22:58:33 +0300

    settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER': 'postgres',
+        'PASSWORD': 'Vipialeki',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
