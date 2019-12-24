593b99d935f1fccbff5aead9ce328cdf46d47500
AlexMuia31 <alexmuia31@gmail.com>
2019-12-24 19:51:34 +0300

    completed the app

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
 
-# Create your views here.
+def allblogs(request):
+    blogs = Blog.objects
+    return render (request, 'allblogs.html',{'blogs':blogs})
+
+def detail(request, blog_id):
+    detailblog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'detail.html',{'blog':detailblog})
