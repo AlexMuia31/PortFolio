7f83c7a2161b15f28cceaa3ee02eb0277d7d222d
AlexMuia31 <alexmuia31@gmail.com>
2020-10-07 12:02:15 +0300

    postgres

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -79,10 +79,15 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER': 'postgres',
+        'PASSWORD': 'Vipialeki1',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
