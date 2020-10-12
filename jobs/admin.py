bdac89553d2ff1bee62bcaded9a0d36039a47913
AlexMuia31 <alexmuia31@gmail.com>
2020-10-12 22:34:40 +0300

    loading pics

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
+
 from .models import Job
 
 admin.site.register(Job)
+
+admin.site.site_header ='Aleki'
+
--- jobs/models.py
+++ jobs/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
 class Job(models.Model):
+    Title=models.CharField(max_length=20)
     image = models.ImageField(upload_to='images/')
     summary =models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.Title
+    
 
 
